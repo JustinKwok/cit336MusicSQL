43c13af091ab40093bc4406ce89e6dc5c68c4668
Justin Kwok <justin.k.kwok@gmail.com>
2017-02-14 17:48:45 -0500

    added comments to queries

--- sqlQueries.sql
+++ sqlQueries.sql
@@ -1,5 +1,11 @@
+/*Grab all artists from NYC, NY*/
+
 SELECT * FROM artists WHERE origin= 'New York City, New York'
 
+/*Grab all artists and order them by name, ascending*/
+
 SELECT * FROM `artists` ORDER BY `artists`.`artistName` ASC
+
+/*Grab all record labels founded before the year 2000*/
 
 SELECT * FROM recordLabel WHERE yearFounded < 2000
